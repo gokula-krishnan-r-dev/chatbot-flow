e0cd8ae282973cba4a710fb51a3bbadef8e8b638
Gokula krishnan R <105733059+gokula-krishnan-r-dev@users.noreply.github.com>
2024-05-24 16:09:17 +0530

    chore: Add Mantine UI library and dependencies

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,19 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
+import React from "react";
+import "./App.css";
+import Dashboard from "./components/dashboard/dashboard";
+import { MantineProvider } from "@mantine/core";
+import { Notifications } from "@mantine/notifications";
+import { DndProvider } from "react-dnd";
+import { HTML5Backend } from "react-dnd-html5-backend";
+const App: React.FC = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <MantineProvider withGlobalStyles withNormalizeCSS>
+      <Notifications position="top-center" />
+      <DndProvider backend={HTML5Backend}>
+        <Dashboard />
+      </DndProvider>
+    </MantineProvider>
   );
-}
+};
 
 export default App;
